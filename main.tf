e1893c911f1462f98eaefd5cbf8180d046057bed
Edwin Ugwu <edwinbuike@gmail.com>
2024-06-26 04:50:55 +0100

    removed engine version for cache so it defaults to latest version

--- main.tf
+++ main.tf
@@ -67,7 +67,7 @@ resource "aws_elasticache_cluster" "redis-db" {
   node_type            = "cache.m4.large"
   num_cache_nodes      = 1
   parameter_group_name = "default.redis3.2"
-  engine_version       = "3.2.10"
+  #engine_version       = "3.2.10"
   port                 = 6379
 
   subnet_group_name  = aws_elasticache_subnet_group.redis-subnet-group.name
